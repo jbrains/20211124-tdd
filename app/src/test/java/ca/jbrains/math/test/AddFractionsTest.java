e6282f89c254a05c1c2aebdadd82a3e2d9627811
J. B. Rainsberger <me@jbrains.ca>
2021-11-25 11:19:23 -0400

    Removed redundant special cases from an if statement.

--- app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -80,12 +80,9 @@ public class AddFractionsTest {
         }
 
         public Fraction plus(Fraction that) {
-            if (this.denominator == that.denominator)
-                return new Fraction(this.numerator + that.numerator, this.denominator);
-            else
-                return new Fraction(
-                        this.numerator * that.denominator + that.numerator * this.denominator,
-                        this.denominator * that.denominator);
+            return new Fraction(
+                    this.numerator * that.denominator + that.numerator * this.denominator,
+                    this.denominator * that.denominator);
         }
 
         @Override
