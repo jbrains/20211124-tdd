cf2b0ee34206d16945e88d104a284730684e0b5e
J. B. Rainsberger <me@jbrains.ca>
2021-11-25 10:36:13 -0400

    We can add _any_ non-integer fractions, as long as they have the same denominator.

--- app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -37,16 +37,23 @@ public class AddFractionsTest {
 
     private static class Fraction {
         private int integerValue;
+        private int numerator;
+        private int denominator;
 
         public Fraction(int integerValue) {
             this.integerValue = integerValue;
         }
 
         public Fraction(int numerator, int denominator) {
+            this.numerator = numerator;
+            this.denominator = denominator;
         }
 
         public Fraction plus(Fraction that) {
-            return new Fraction(this.integerValue + that.integerValue);
+            if (this.denominator == 0)
+                return new Fraction(this.integerValue + that.integerValue);
+            else
+                return new Fraction(this.numerator + that.numerator, this.denominator);
         }
 
         public int intValue() {
@@ -54,11 +61,11 @@ public class AddFractionsTest {
         }
 
         public int getNumerator() {
-            return 3;
+            return numerator;
         }
 
         public int getDenominator() {
-            return 5;
+            return denominator;
         }
     }
 }
