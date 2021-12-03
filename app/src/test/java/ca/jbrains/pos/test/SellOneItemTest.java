334c1f481e08951af40725b69f6c518a63d2dd86
J. B. Rainsberger <me@jbrains.ca>
2021-12-03 09:23:58 -0400

    We can sell one item, as long as we know the price; however, the price of products that we don't even know is EUR 12.50.

--- app/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ app/src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -6,22 +6,48 @@ import org.junit.jupiter.api.Test;
 public class SellOneItemTest {
     @Test
     void productFound() {
-        Sale sale = new Sale();
         Display display = new Display();
+        Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
 
         Assertions.assertEquals("EUR 7.95", display.getText());
     }
 
+    @Test
+    void anotherProductFound() {
+        Display display = new Display();
+        Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+
+        Assertions.assertEquals("EUR 12.50", display.getText());
+    }
+
     private static class Sale {
+        private Display display;
+
+        private Sale(Display display) {
+            this.display = display;
+        }
+
         public void onBarcode(String barcode) {
+            if ("12345".equals(barcode))
+                display.setText("EUR 7.95");
+            else
+                display.setText("EUR 12.50");
         }
     }
 
     private static class Display {
+        private String text;
+
         public String getText() {
-            return "EUR 7.95";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 }
