036c352dd910db410be930ee6bfa95d44cc14572
J. B. Rainsberger <me@jbrains.ca>
2021-12-07 11:03:58 -0400

    The ConsoleDisplay no longer talks directly to a global output stream.

--- app/src/test/java/ca/jbrains/pos/test/ConsoleDisplay.java
+++ app/src/test/java/ca/jbrains/pos/test/ConsoleDisplay.java
@@ -5,23 +5,25 @@ import java.io.StringWriter;
 
 public class ConsoleDisplay implements Display {
     private StringWriter canvas;
+    private PrintWriter out;
 
     public ConsoleDisplay(StringWriter canvas) {
         this.canvas = canvas;
+        this.out = new PrintWriter(canvas);
     }
 
     @Override
     public void displayPrice(Price price) {
-        new PrintWriter(canvas).println(String.format("EUR %.2f", price.valueInCents() / 100.0d));
+        out.println(String.format("EUR %.2f", price.valueInCents() / 100.0d));
     }
 
     @Override
     public void displayProductNotFoundMessage(String barcodeNotFound) {
-        System.out.println(String.format("Product not found: %s", barcodeNotFound));
+        out.println(String.format("Product not found: %s", barcodeNotFound));
     }
 
     @Override
     public void displayEmptyBarcodeMessage() {
-        System.out.println("Scanning error: empty barcode");
+        out.println("Scanning error: empty barcode");
     }
 }
