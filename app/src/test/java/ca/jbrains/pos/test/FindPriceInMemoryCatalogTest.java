821591af90d2523c74e82c7f85edafdeac5dc3e5
J. B. Rainsberger <me@jbrains.ca>
2021-12-07 10:30:32 -0400

    Confirmed that the InMemory Catalog correctly handles not finding the price for a barcode.

--- app/src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ app/src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -3,6 +3,7 @@ package ca.jbrains.pos.test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class FindPriceInMemoryCatalogTest {
@@ -10,6 +11,12 @@ public class FindPriceInMemoryCatalogTest {
     void productFound() {
         InMemoryCatalog catalog = new InMemoryCatalog(Map.of("23456", Price.cents(1250)));
         Assertions.assertEquals(Price.cents(1250), catalog.findPrice("23456"));
+    }
+
+    @Test
+    void productNotFound() {
+        InMemoryCatalog catalog = new InMemoryCatalog(Collections.emptyMap());
+        Assertions.assertEquals(null, catalog.findPrice("23456"));
     }
 
     public static class InMemoryCatalog {
