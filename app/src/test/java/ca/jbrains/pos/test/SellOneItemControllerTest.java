1eb596d4692034b5327210e9d08ac6a4d1c93188
J. B. Rainsberger <me@jbrains.ca>
2021-12-07 08:50:52 -0400

    Sketched a test.

---
+++ app/src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
@@ -0,0 +1,35 @@
+package ca.jbrains.pos.test;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class SellOneItemControllerTest {
+    @Test
+    void productFound() {
+        Catalog catalog = Mockito.mock(Catalog.class);
+        Display display = Mockito.mock(Display.class);
+
+        Mockito.when(catalog.findPrice("12345")).thenReturn(Price.cents(795));
+
+        new SellOneItemController(catalog, display).onBarcode("12345");
+
+        Mockito.verify(display).displayPrice(Price.cents(795));
+    }
+    public static class SellOneItemController {
+        public SellOneItemController(Catalog catalog, Display display) {
+        }
+
+        public void onBarcode(String barcode) {
+        }
+    }
+    public interface Catalog {
+        Price findPrice(String barcode);
+    }
+    public interface Display {
+        void displayPrice(Price price);
+    }
+
+    public record Price(int valueInCents) {
+        public static Price cents(int valueInCents) { return new Price(valueInCents); }
+    }
+}
